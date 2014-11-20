3dde948fc37b12105ca24e6aec8335eed0fb75f1
Zeraan <brent.c.patterson@gmail.com>
2014-11-20 13:09:39 -0700

    Changing from scrollbars to sliders to make it more touch-centric UI.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,26 +26,31 @@ namespace MazeRunner
 			Game game = new Game();
 			DataContext = game;
 			MazeCanvas.Level = game.CurrentLevel;
-			horizontalScrollbar.Maximum = MazeCanvas.Level.Width - 17;
-			verticalScrollbar.Maximum = MazeCanvas.Level.Height - 17;
-		}
-
-		private void horizontalScrollbar_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
-		{
-			MazeCanvas.Left = (int)e.NewValue;
-			MazeCanvas.InvalidateVisual();
-		}
-
-		private void verticalScrollbar_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
-		{
-			MazeCanvas.Top = (int)e.NewValue;
-			MazeCanvas.InvalidateVisual();
+			horizontalScrollbar.Maximum = MazeCanvas.Level.Width - 16;
+			verticalScrollbar.Maximum = MazeCanvas.Level.Height - 16;
+			verticalScrollbar.Value = verticalScrollbar.Maximum;
 		}
 
 		private void _mazeRunnerWindow_SizeChanged(object sender, SizeChangedEventArgs e)
 		{
 			horizontalScrollbar.Maximum = MazeCanvas.Level.Width - MazeCanvas.ActualWidth / 32 + 1;
 			verticalScrollbar.Maximum = MazeCanvas.Level.Height - MazeCanvas.ActualHeight / 32 + 1;
+		}
+
+		private void horizontalScrollbar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			MazeCanvas.Left = (int)e.NewValue;
+			MazeCanvas.InvalidateVisual();
+		}
+
+		private void verticalScrollbar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			var slider = sender as Slider;
+			if (sender != null)
+			{
+				MazeCanvas.Top = (int)(slider.Maximum - e.NewValue);
+				MazeCanvas.InvalidateVisual();
+			}
 		}
 	}
 }
